f41e1881ee8f89e8b40513c32abb4cd73f37ae12
atish <atish.raina91@gmail.com>
2024-11-21 11:50:13 +0530

    Add initial code

---
+++ v-dom/index.js
@@ -0,0 +1,13 @@
+const vDom = {
+    type: 'ul',
+    children : {
+        type: 'li',
+        value: '#1',
+    }
+}
+
+const main = document.getElementById('main');
+
+function appendDom(vDom, dom) {
+    const element = 
+}
